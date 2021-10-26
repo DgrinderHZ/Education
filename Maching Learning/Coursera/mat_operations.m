1f470d040044203237099dc3b205d28ee86ac87e
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2021-10-26 22:28:55 +0100

    Create mat_operations.m

---
+++ Maching Learning/Coursera/mat_operations.m
@@ -0,0 +1,17 @@
+% Initialize
+A = [1, 2, 4; 5, 3, 2]
+B = [1, 3, 4; 1, 1, 1]
+
+% Set the s constant
+s = 2
+
+% Element-wise Addition/Substraction 
+A_plus_B = A + B
+A_minus_B = A - B
+
+% Scalar Multiplication/Division
+A_times_s = A * s
+A_div_s = A / s
+
+% What happens if we have a Matrix + scalar?
+add_As = A + s
