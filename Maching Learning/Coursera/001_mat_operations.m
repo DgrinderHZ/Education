6d8662796d3dc2c9b85aeab11763b516b2d4d31f
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2021-10-26 22:45:44 +0100

    Update 001_mat_operations.m

--- Maching Learning/Coursera/001_mat_operations.m
+++ Maching Learning/Coursera/001_mat_operations.m
@@ -1,3 +1,4 @@
+% Run it online here: octav.onl/ml_scripts
 % Initialize
 A = [1, 2, 4; 5, 3, 2]
 B = [1, 3, 4; 1, 1, 1]
@@ -15,3 +16,12 @@ A_div_s = A / s
 
 % What happens if we have a Matrix + scalar?
 add_As = A + s
+
+% Initialize matrix C 
+C = [1, 2, 3; 4, 5, 6;7, 8, 9] 
+
+% Initialize vector v 
+v = [1; 1; 1] 
+
+% Multiply A * v
+Cv = C * v
