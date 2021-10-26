46643e3a5d92641163836e8758fa6e9dc5de96d5
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2021-10-27 00:37:45 +0100

    Create 002_linear_algebra_assignment.m

---
+++ Maching Learning/Coursera/002_linear_algebra_assignment.m
@@ -0,0 +1,21 @@
+% Linear Algebra Assignment: Run it here octav.onl/ml_scripts
+
+disp('Q1:')
+A =  [1, -4; -2, 1];
+B = [0, 3; 5, 8];
+A + B
+
+disp('Q2:')
+x =  [5;5;2;7];
+2*x
+
+disp('Q3:')
+u =  [8;1;4];
+u'
+
+disp('Q4:')
+u =  [4;-4;-3];
+v =  [4;2;4];
+u'*v
+
+disp('Q4: Theoritical!')
